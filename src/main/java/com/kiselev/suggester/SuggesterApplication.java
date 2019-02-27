8a0e1764dcd27b9c760fff943438b6597ef66b4d
Vadim Kiselev <vadim8kiselev@gmail.com>
2019-02-27 15:37:28 +0400

    Initial commit

---
+++ src/main/java/com/kiselev/suggester/SuggesterApplication.java
@@ -0,0 +1,12 @@
+package com.kiselev.suggester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuggesterApplication.class, args);
+    }
+}
